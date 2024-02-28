976758eb350ea2b2606d014ac0880f687ac1a43d
Tejas <tejas9075674655@gmail.com>
2024-02-28 21:46:19 +0530

    DOM PROJECT 1

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var targetDiv = document.getElementById("targetDiv");
+
+    targetDiv.style.backgroundColor = "yellow";
+    targetDiv.style.margin = "20px"; 
+    targetDiv.style.padding = "10px"; 
+    targetDiv.style.fontSize = "18px"; 
+    targetDiv.style.fontWeight = "bold"; 
+    targetDiv.style.height = "200px"; 
+    targetDiv.style.width = "300px"; 
+});
